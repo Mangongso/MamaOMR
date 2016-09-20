e08b331f6de1cfb56b2d843a46af04c2e928bb0e
inkukyang <inkuk.yang@hanbnc.com>
2016-09-20 15:43:48 +0900

    오답노트 수정 모델

--- Model/ManGong/WrongNote.php
+++ Model/ManGong/WrongNote.php
@@ -139,12 +139,27 @@ class WrongNote{
 		$boolResult = $this->resWrongDB->DB_access($this->resWrongDB,$strQuery);
 		return($boolResult);		
 	}
-	public function getWrongAnswerNoteFromTest($intTestSeq,$intRecordSeq,$intStudentSeq){
+	public function getWrongAnswerNoteFromTest($intTestSeq,$intRecordSeq=null,$intStudentSeq=null){
+		$strInnerQuery = sprintf(" SELECT * FROM user_answer WHERE delete_flg=0 AND test_seq=%d ",$intTestSeq);
+		if(!is_null($intRecordSeq)){
+			$strInnerQuery .= sprintf(" AND record_seq=%d ",$intRecordSeq);
+		}
+		if(!is_null($intStudentSeq)){
+			$strInnerQuery .= sprintf(" AND user_seq=%d ",$intStudentSeq);
+		}
 		$strQuery = sprintf("
-				select r1.*,tql.order_number from (
-				select ua.*,wn.seq as wrong_note_list_seq,wn.create_date as wrong_note_date from user_answer ua left outer join wrong_note_list wn on ua.test_seq=wn.test_seq and ua.record_seq=wn.record_seq and ua.user_seq=wn.user_seq and ua.question_seq=wn.question_seq where ua.test_seq=%d and ua.record_seq=%d and ua.user_seq=%d and ua.result_flg=0
-				) as r1 left join test_question_list tql on r1.test_seq=tql.test_seq and r1.question_seq=tql.question_seq
-				",$intTestSeq,$intRecordSeq,$intStudentSeq);
+				SELECT r1.*,tql.order_number 
+				FROM ( SELECT ua.*,wn.seq AS wrong_note_list_seq,wn.create_date AS wrong_note_date,wn.file_name,wn.question AS question_contents
+					FROM (".$strInnerQuery." group by question_seq having result_flg=0) ua 
+					LEFT OUTER JOIN wrong_note_list wn ON ua.test_seq=wn.test_seq 
+					/*AND ua.record_seq=wn.record_seq */
+					AND ua.user_seq=wn.user_seq 
+					AND ua.question_seq=wn.question_seq 
+					) AS r1 
+					LEFT JOIN test_question_list tql ON r1.test_seq=tql.test_seq AND r1.question_seq=tql.question_seq
+					ORDER BY order_number
+				",$intTestSeq);
+		//print_r($strQuery);exit;
 		$arrResult = $this->resWrongDB->DB_access($this->resWrongDB,$strQuery);
 		return($arrResult);						
 	}
