f01f811fd5c7239cf6677ceafd3fa8647178e5d6
inkukyang <inkuk.yang@hanbnc.com>
2016-09-27 20:03:16 +0900

    테스트 결과 페이지 모바일  메뉴 수정

--- View/smart_omr/exercise_book/test_result.php
+++ View/smart_omr/exercise_book/test_result.php
@@ -42,31 +42,34 @@
 					<div
 						class="pure-menu pure-menu-horizontal pure-menu-scrollable scrollable-menu  visible-xs mypage_menu">
 						<ul id="foot_menu-tabs2" class="pure-menu-list">
-							<li class="pure-menu-item"><a 
-					onclick="$('.sub_content_top_menu li').attr('class','');$(this).attr('class','active');objCommon.displayTab('study_report');"><a
-					href="javascript:void(0);" title="학습리포트"><i
-						class="uk-icon-caret-right"></i> 학습리포트</a></li>
-							<li class="pure-menu-item"><a onclick="$('.sub_content_top_menu li').attr('class','');$(this).attr('class','active');objCommon.displayTab('answer_omr');"><a
-					href="javascript:void(0);" title="입력답안"><i
-						class="uk-icon-caret-right"></i> 입력답안</a></li>
-							<li class="pure-menu-item"><a onclick="$('.sub_content_top_menu li').attr('class','');$(this).attr('class','active');objCommon.displayTab('wrong_answer_note');"><a
-					href="javascript:void(0);" title="오답노트"><i
-						class="uk-icon-caret-right"></i> 오답노트</a></li>
-							<li class="pure-menu-item"><a onclick="$('.sub_content_top_menu li').attr('class','');$(this).attr('class','active');objCommon.displayTab('wrong_answer_test');">
-					<a href="javascript:void(0);" title="오답문제풀이"><i
-						class="uk-icon-caret-right"></i> 오답문제풀이 <small><i
-							onclick="objWAN.getWrongNoteTest('<?=$_GET['t']?>','<?=$_GET['revision']?>');"
-							style="cursor: pointer; color: #ccc;" class="fa fa-refresh"
-							aria-hidden="true" t="<?=$_GET['t']?>"
-							revision="<?=$_GET['revision']?>"></i></small></a></li>
-									<li class="pure-menu-item"><a onclick="$('.sub_content_top_menu li').attr('class','');$(this).attr('class','active');objCommon.displayTab('comment');">
-					<a href="javascript:void(0);" title="댓글"><i
-						class="uk-icon-caret-right"></i> 
-						댓글 <small><i
-							onclick="objCommon.getComment($('#comment_div').attr('comment_seq'),$('#comment_div').attr('bbs_seq'));"
-							style="cursor: pointer; color: #ccc;" class="fa fa-refresh"
-							aria-hidden="true" t="<?=$_GET['t']?>"
-							revision="<?=$_GET['revision']?>"></i></a></li>
+							<li class="pure-menu-item">
+								<a href="#" title="학습리포트" onclick="$('.sub_content_top_menu li').attr('class','');$(this).attr('class','active');objCommon.displayTab('answer_omr');">
+									<i class="uk-icon-caret-right"></i> 학습리포트
+								</a>
+							</li>
+							<li class="pure-menu-item">
+								<a href="#" title="입력답안" onclick="$('.sub_content_top_menu li').attr('class','');$(this).attr('class','active');objCommon.displayTab('study_report');">
+									<i class="uk-icon-caret-right"></i> 입력답안
+								</a>
+							</li>
+							<li class="pure-menu-item">
+								<a href="#" title="오답노트" onclick="$('.sub_content_top_menu li').attr('class','');$(this).attr('class','active');objCommon.displayTab('wrong_answer_note');">
+									<i class="uk-icon-caret-right"></i> 오답노트
+								</a>
+							</li>
+							<li class="pure-menu-item">
+								<a href="#" title="오답문제풀이" onclick="$('.sub_content_top_menu li').attr('class','');$(this).attr('class','active');objCommon.displayTab('wrong_answer_test');">
+									<i class="uk-icon-caret-right"></i> 오답문제풀이 
+									<small>
+										<i onclick="objWAN.getWrongNoteTest('<?=$_GET['t']?>','<?=$_GET['revision']?>');" style="cursor: pointer; color: #ccc;" class="fa fa-refresh" aria-hidden="true" data-t="<?=$_GET['t']?>" data-revision="<?=$_GET['revision']?>"></i>
+									</small>
+								</a>
+							</li>
+							<li class="pure-menu-item">
+								<a href="#" title="댓글" onclick="$('.sub_content_top_menu li').attr('class','');$(this).attr('class','active');objCommon.displayTab('comment');objCommon.getComment($('#comment_div').attr('data-comment-seq'),$('#comment_div').attr('data-bbs-seq'));">
+									<i class="uk-icon-caret-right"></i> 댓글 
+								</a>
+							</li>
 						</ul>
 					</div>
 			
@@ -97,15 +100,8 @@
 				</a>
 				</li>
 				<li class=""
-					onclick="$('.sub_content_top_menu li').attr('class','');$(this).attr('class','active');objCommon.displayTab('comment');">
-					<a href="#" title="댓글"> <i
-						class="fa fa-comment fa-flip-horizontal" aria-hidden="true"></i>
-						댓글 <small><i
-							onclick="objCommon.getComment($('#comment_div').attr('data-comment-seq'),$('#comment_div').attr('data-bbs-seq'));"
-							style="cursor: pointer; color: #ccc;" class="fa fa-refresh"
-							aria-hidden="true" data-t="<?=$_GET['t']?>"
-							data-revision="<?=$_GET['revision']?>"></i></small>
-				</a>
+					onclick="$('.sub_content_top_menu li').attr('class','');$(this).attr('class','active');objCommon.displayTab('comment');objCommon.getComment($('#comment_div').attr('data-comment-seq'),$('#comment_div').attr('data-bbs-seq'));">
+					<a href="#" title="댓글"> <i class="fa fa-comment fa-flip-horizontal" aria-hidden="true"></i>댓글 </a>
 				</li>
 			</ul>
 
